baab5496d11cb43569c58839216d62ff2dd757e6
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2021-03-06 20:04:02 +0100

    Add basic React structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+
+import logo from '../assets/images/logo.svg';
+import default_graph from '../assets/data/demo_anedor_graph_withEdgePoints.vtu'
+
+function App() {
+  return (
+      <div className="App">
+          <h2>Hola</h2>
+      </div>
+  );
+}
+
+export default App;
